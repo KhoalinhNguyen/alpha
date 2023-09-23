5f26af4e6eb92756a854caa059421b975e34d647
KhoalinhNguyen <khoalinhnguyen123@gmail.com>
2023-09-23 22:15:20 +0200

    cut out frontend

--- src/main/resources/Frontend/alpha-app/src/app/user.model.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    id!: number;
-    firstName!: string;
-    lastName!: string;
-    email!: string;
-    phoneNumber!: string;
-    currentPosition!: string;
-    department!:string;
-}
